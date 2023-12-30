9875e9cf335f4fa1a039820b6c9bf269f03f0db9
benjamin <benjaminlikita3@gmail.com>
2023-12-30 11:21:38 +0100

    Updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -16,7 +16,7 @@ function Navbar({color = "text-black", border = "border-gray-200", opacity = "bg
     <div className={`bg-transparent border-b ${border} shadow-sm ${color} bg-black ${opacity}`}>
         <div className='w-[90%] flex justify-between items-center m-auto md:p-5 py-3'>
           
-          <Link href={"/"} className='text-3xl md:text-4xl font-semibold'>Logo</Link>
+          <Link href={"/"} className='text-4xl font-semibold'>Logo</Link>
 
           <nav className={clsx('justify-between font-medium text-xl md:text-lg block md:flex bg-gray-500 bg-opacity-95 w-[60%] md:w-[55%] lg:w-[40%] text-gray-700 md:text-inherit h-full transition-all duration-500 md:bg-transparent fixed md:relative top-0 p-10 pl-5 pt-14 md:p-0', {
             ['right-0 bg-white z-50 p-10']: visible,
@@ -27,9 +27,9 @@ function Navbar({color = "text-black", border = "border-gray-200", opacity = "bg
               className={"block md:hidden absolute top-[16px] right-[25px] cursor-pointer"}
               onClick={() => setVisibility(false)}
             />
-            <Link href={"/"}className={`py-3 px-5 block md:inline-block lg:hover:scale-110 transition-all duration-700 ${pathname === '/' && 'active'}`}>Home</Link>
-            <Link href={"/gallery"} className={`py-3 px-5 block md:inline-block lg:hover:scale-110 transition-all duration-700 ${pathname === '/gallery' && 'active'}`}>Gallery</Link>
-            <Link href={"/contact"} className={`py-3 px-5 block md:inline-block lg:hover:scale-110 transition-all duration-700 ${pathname === '/contact' && 'active'}`}>Contact Us</Link>
+            <Link href={"/"}className={`py-3 px-5 my-2 md:my-0 block md:inline-block lg:hover:scale-110 transition-all duration-700 ${pathname === '/' && 'active'}`}>Home</Link>
+            <Link href={"/gallery"} className={`py-3 px-5 my-2 md:my-0 block md:inline-block lg:hover:scale-110 transition-all duration-700 ${pathname === '/gallery' && 'active'}`}>Gallery</Link>
+            <Link href={"/contact"} className={`py-3 px-5 my-2 md:my-0 block md:inline-block lg:hover:scale-110 transition-all duration-700 ${pathname === '/contact' && 'active'}`}>Contact Us</Link>
           </nav>
 
           <div className='block md:hidden'>
