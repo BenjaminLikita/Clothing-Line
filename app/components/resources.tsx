734af3be729a6c808c8e9264783b0f483238e3b1
benjamin <benjaminlikita3@gmail.com>
2024-01-13 17:14:53 +0100

    Updated

--- app/components/resources.tsx
+++ app/components/resources.tsx
@@ -1,12 +1,31 @@
+import next from "next";
+import { CloudinaryResource } from "../gallery/page";
 
-const fetchImages = async () => {
+const fetchImages = async (results: CloudinaryResource[], nextCursor: string | null) => {
+    "use client"
     try{
-        const result = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, {
+        const baseUrl = `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`
+        const url = nextCursor ? `${baseUrl}?next_cursor=${nextCursor}` : baseUrl;
+
+        const result = await fetch(url, {
             headers : {
             Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_CLOUD_API_KEY + ":" + process.env.CLOUDINARY_CLOUD_API_SECRET).toString('base64')}`
-        }})
+        }, next: { revalidate: 3600 }})
+        
         const data = await result.json()
-        return data.resources
+
+
+        data.resources.map((res: CloudinaryResource) => {
+            results.push(res)
+        })
+
+        if(data.next_cursor){
+            await fetchImages(results, data.next_cursor)
+        } else{
+            return results
+        }
+        return results
+        
     }
     catch(err){
         console.error("Error Fetching Images:", err)
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -20,11 +20,12 @@ export interface CloudinaryResource {
   public_id: string;
   secure_url: string;
 }
+
 async function GalleryPage() {
   
-  
-  const resources = await fetchImages();
-  console.log(resources)
+  const results: CloudinaryResource[] = []
+  const data = await fetchImages(results, "");
+  console.log(data)
   
   
   return (
@@ -32,23 +33,24 @@ async function GalleryPage() {
       <Navbar />
       <div className='w-[90%] m-auto'>
         <h1 className='my-5 text-4xl md:text-6xl text-center font-bold'>Gallery</h1>
-        <p className='bg-gray-900 text-white w-max py-3 px-5 rounded-3xl font-bold'>Photos <span className='pl-5 text-gray-500 font-normal'>{resources.length}</span></p>
-        {/* <div className={`grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-[100%] my-7 gap-5`}> */}
+        <p className='bg-gray-900 text-white w-max py-3 px-5 rounded-3xl font-bold'>Photos <span className='pl-5 text-gray-500 font-normal'>{data.length}</span></p>
+        {/* <div className={`flex flex-wrap sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-red-500 w-[100%] my-7 gap-5`}> */}
         <div className={`grid grid-cols-2 lg:grid-cols-3 w-[100%] my-7 gap-5`}>
           {
-            resources.map((item: CloudinaryResource, i: number) => (
+            data.map((item: CloudinaryResource, i: number) => (
               <div
               key={item.public_id}
-              className={clsx(`cursor-pointer overflow-hidden rounded-xl`, {
+              // className={clsx(`cursor-pointer overflow-hidden bg-green-500 h-[400px]`, {
+              className={clsx(`cursor-pointer overflow-hidden`, {
                 [`row-span-2`]: i % 2,
               })}
               >
                 <Image
                   src={item.secure_url}
-                  width="300"
-                  height='250'
+                  width="30"
+                  height='25'
                   alt={item.public_id}
-                  className={clsx(`block object-cover img hover:scale-110 transition-all duration-500`, {
+                  className={clsx(`block rounded-xl h-[100%] w-[100%] object-cover hover:scale-110 transition-all duration-500`, {
                     // [`lg:row-span-2 md:col-span-2 lg:col-span-1`]: i % 2,
                   })}
                   loading='lazy'
